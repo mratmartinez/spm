ebe54482d717d9aa142fc5b39d3b4ff142e96b2b
Juan Martínez <mratmartinez@anche.no>
2017-05-26 07:48:48 -0300

    Added some dependencies for checking
    Also now the CheckDeps function will check beforehand if there isn't any
    dependency.

--- scripts/10-funcs.sh
+++ scripts/10-funcs.sh
@@ -3,14 +3,17 @@ function InstallPKG {
 }
 
 function CheckDeps {
-    for depitem in ${dependencies[*]}
-    do
-        if [ ! -d ${IPLACE}/${depitem} ]
-        then
-            echo ${depitem} is absent
-            source ${PORTS}/${depitem}/full
-        else
-            echo ${deptest} is installed!
-        fi
-    done
+    if [ ! ${#dependencies[@]} -eq 0 ]
+    then
+        for depitem in ${dependencies[*]}
+        do
+            if [ ! -d ${IPLACE}/${depitem} ]
+            then
+                echo ${depitem} is absent
+                source ${PORTS}/${depitem}/full
+            else
+                echo ${deptest} is installed!
+            fi
+        done
+    fi
 }
