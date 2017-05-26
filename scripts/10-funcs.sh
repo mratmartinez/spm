fa0ee7d1dd671fa14858f333fd3cf30ad34f13c4
Juan Martínez <mratmartinez@anche.no>
2017-05-26 01:10:30 -0300

    Now we have a function for checking dependencies.
    The function CheckDeps search in an array for dependencies.

--- scripts/10-funcs.sh
+++ scripts/10-funcs.sh
@@ -1,3 +1,16 @@
 function InstallPKG {
     source ${PORTS}/${1}/full
 }
+
+function CheckDeps {
+    for depitem in ${dependencies[*]}
+    do
+        if [ ! -d ${IPLACE}/${depitem} ]
+        then
+            echo ${depitem} is absent
+            source ${PORTS}/${depitem}/full
+        else
+            echo ${deptest} is installed!
+        fi
+    done
+}
